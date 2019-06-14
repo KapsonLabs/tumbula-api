08c987a191ecf6966e7c77738fe323fadd99e71f
Katongole Allan <kapsonkatongole@gmail.com>
2019-06-14 19:19:06 +0300

    Initial Commit

---
+++ tumbula_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/allan-only/projects/blockchain/final_project/tumbula-api/tumbula_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
